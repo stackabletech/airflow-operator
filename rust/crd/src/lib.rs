7770e9454db8c6b88fb80ceb85fa91695d23364c
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-01-10 15:22:02 +0100

    clone based on superset code

---
+++ rust/crd/src/lib.rs
@@ -0,0 +1,152 @@
+pub mod commands;
+
+use std::collections::BTreeMap;
+
+use serde::{Deserialize, Serialize};
+use stackable_operator::kube::runtime::reflector::ObjectRef;
+use stackable_operator::kube::CustomResource;
+use stackable_operator::product_config_utils::{ConfigError, Configuration};
+use stackable_operator::role_utils::{Role, RoleGroupRef};
+use stackable_operator::schemars::{self, JsonSchema};
+use strum_macros::Display;
+use strum_macros::EnumIter;
+
+pub const APP_NAME: &str = "airflow";
+pub const MANAGED_BY: &str = "airflow-operator";
+
+pub const HTTP_PORT: &str = "http";
+
+#[derive(Clone, CustomResource, Debug, Default, Deserialize, JsonSchema, PartialEq, Serialize)]
+#[kube(
+    group = "airflow.stackable.tech",
+    version = "v1alpha1",
+    kind = "AirflowCluster",
+    plural = "airflowclusters",
+    shortname = "airflow",
+    status = "AirflowClusterStatus",
+    namespaced,
+    crates(
+        kube_core = "stackable_operator::kube::core",
+        k8s_openapi = "stackable_operator::k8s_openapi",
+        schemars = "stackable_operator::schemars"
+    )
+)]
+#[serde(rename_all = "camelCase")]
+pub struct AirflowClusterSpec {
+    /// Emergency stop button, if `true` then all pods are stopped without affecting configuration (as setting `replicas` to `0` would)
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub stopped: Option<bool>,
+    /// Desired Airflow version
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub version: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub nodes: Option<Role<AirflowConfig>>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AirflowCredentials {
+    pub admin_user: AdminUserCredentials,
+    pub connections: Connections,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AdminUserCredentials {
+    pub username: String,
+    pub firstname: String,
+    pub lastname: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Connections {
+    pub secret_key: String,
+    pub sqlalchemy_database_uri: String,
+}
+
+#[derive(
+    Clone, Debug, Deserialize, Display, EnumIter, Eq, Hash, JsonSchema, PartialEq, Serialize,
+)]
+pub enum AirflowRole {
+    #[strum(serialize = "node")]
+    Node,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AirflowConfig {
+    pub credentials_secret: String,
+}
+
+impl AirflowConfig {
+    pub const CREDENTIALS_SECRET_PROPERTY: &'static str = "credentialsSecret";
+}
+
+impl Configuration for AirflowConfig {
+    type Configurable = AirflowCluster;
+
+    fn compute_env(
+        &self,
+        _resource: &Self::Configurable,
+        _role_name: &str,
+    ) -> Result<BTreeMap<String, Option<String>>, ConfigError> {
+        Ok([(
+            Self::CREDENTIALS_SECRET_PROPERTY.to_string(),
+            Some(self.credentials_secret.clone()),
+        )]
+        .into())
+    }
+
+    fn compute_cli(
+        &self,
+        _resource: &Self::Configurable,
+        _role_name: &str,
+    ) -> Result<BTreeMap<String, Option<String>>, ConfigError> {
+        Ok(BTreeMap::new())
+    }
+
+    fn compute_files(
+        &self,
+        _resource: &Self::Configurable,
+        _role_name: &str,
+        _file: &str,
+    ) -> Result<BTreeMap<String, Option<String>>, ConfigError> {
+        Ok(BTreeMap::new())
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AirflowClusterStatus {}
+
+impl AirflowCluster {
+    /// The name of the role-level load-balanced Kubernetes `Service`
+    pub fn node_role_service_name(&self) -> Option<String> {
+        self.metadata.name.clone()
+    }
+
+    /// Metadata about a node rolegroup
+    pub fn node_rolegroup_ref(
+        &self,
+        group_name: impl Into<String>,
+    ) -> RoleGroupRef<AirflowCluster> {
+        RoleGroupRef {
+            cluster: ObjectRef::from_obj(self),
+            role: AirflowRole::Node.to_string(),
+            role_group: group_name.into(),
+        }
+    }
+}
+
+/// A reference to a [`AirflowCluster`]
+#[derive(Clone, Default, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AirflowClusterRef {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub namespace: Option<String>,
+}
---
+++ rust/operator-binary/src/main.rs
@@ -0,0 +1,118 @@
+mod airflow_controller;
+mod init_controller;
+
+use futures::StreamExt;
+use stackable_airflow_crd::{commands::Init, AirflowCluster};
+use stackable_operator::{
+    cli::Command,
+    k8s_openapi::api::{apps::v1::StatefulSet, core::v1::Service},
+    kube::{
+        api::{DynamicObject, ListParams},
+        runtime::{
+            controller::{Context, ReconcilerAction},
+            reflector::ObjectRef,
+            Controller,
+        },
+        CustomResourceExt, Resource,
+    },
+};
+use structopt::StructOpt;
+
+mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+pub const APP_NAME: &str = "airflow";
+pub const APP_PORT: u16 = 8088;
+
+#[derive(StructOpt)]
+#[structopt(about = built_info::PKG_DESCRIPTION, author = stackable_operator::cli::AUTHOR)]
+struct Opts {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+/// Erases the concrete types of the controller result, so that we can merge the streams of multiple controllers for different resources.
+///
+/// In particular, we convert `ObjectRef<K>` into `ObjectRef<DynamicObject>` (which carries `K`'s metadata at runtime instead), and
+/// `E` into the trait object `anyhow::Error`.
+fn erase_controller_result_type<K: Resource, E: std::error::Error + Send + Sync + 'static>(
+    res: Result<(ObjectRef<K>, ReconcilerAction), E>,
+) -> anyhow::Result<(ObjectRef<DynamicObject>, ReconcilerAction)> {
+    let (obj_ref, action) = res?;
+    Ok((obj_ref.erase(), action))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    stackable_operator::logging::initialize_logging("AIRFLOW_OPERATOR_LOG");
+
+    let opts = Opts::from_args();
+    match opts.cmd {
+        Command::Crd => println!(
+            "{}{}",
+            serde_yaml::to_string(&AirflowCluster::crd())?,
+            serde_yaml::to_string(&Init::crd())?
+        ),
+        Command::Run { product_config } => {
+            stackable_operator::utils::print_startup_string(
+                built_info::PKG_DESCRIPTION,
+                built_info::PKG_VERSION,
+                built_info::GIT_VERSION,
+                built_info::TARGET,
+                built_info::BUILT_TIME_UTC,
+                built_info::RUSTC_VERSION,
+            );
+            let product_config = product_config.load(&[
+                "deploy/config-spec/properties.yaml",
+                "/etc/stackable/airflow-operator/config-spec/properties.yaml",
+            ])?;
+            let client = stackable_operator::client::create_client(Some(
+                "airflow.stackable.tech".to_string(),
+            ))
+            .await?;
+            let airflow_controller = Controller::new(
+                client.get_all_api::<AirflowCluster>(),
+                ListParams::default(),
+            )
+            .owns(client.get_all_api::<Service>(), ListParams::default())
+            .owns(client.get_all_api::<StatefulSet>(), ListParams::default())
+            .run(
+                airflow_controller::reconcile_airflow,
+                airflow_controller::error_policy,
+                Context::new(airflow_controller::Ctx {
+                    client: client.clone(),
+                    product_config,
+                }),
+            );
+
+            let init_controller =
+                Controller::new(client.get_all_api::<Init>(), ListParams::default()).run(
+                    init_controller::reconcile_init,
+                    init_controller::error_policy,
+                    Context::new(init_controller::Ctx {
+                        client: client.clone(),
+                    }),
+                );
+
+            futures::stream::select(
+                airflow_controller.map(erase_controller_result_type),
+                init_controller.map(erase_controller_result_type),
+            )
+            .for_each(|res| async {
+                match res {
+                    Ok((obj, _)) => tracing::info!(object = %obj, "Reconciled object"),
+                    Err(err) => {
+                        tracing::error!(
+                            error = &*err as &dyn std::error::Error,
+                            "Failed to reconcile object",
+                        )
+                    }
+                }
+            })
+            .await;
+        }
+    }
+
+    Ok(())
+}
