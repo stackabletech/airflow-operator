94a9a873be32af2ef9036e927edd7ea337728667
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-04-28 18:27:23 +0200

    some lint fixes

--- tests/templates/kuttl/smoke/metrics.py
+++ tests/templates/kuttl/smoke/metrics.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python
 
 import requests
-import sys
 import time
+
 
 def assert_metric(role, metric):
     response = requests.get(f'http://airflow-{role}-default:9102/metrics')
     assert response.status_code == 200, \
-            f"Metrics could not be retrieved from the {role}."
+        f"Metrics could not be retrieved from the {role}."
     assert metric in response.text, \
-            f"The {role} metrics do not contain the metric {metric}."
+        f"The {role} metrics do not contain the metric {metric}."
+
 
 # Trigger a DAG run to create metrics
 dag_id = 'example_trigger_target_dag'
-dag_conf = { 'message': "Hello World" }
+dag_conf = {'message': "Hello World"}
 
 rest_url = 'http://airflow-webserver-default:8080/api/v1'
 auth = ('airflow', 'airflow')
 
-response = requests.patch(
-        f'{rest_url}/dags/{dag_id}',
-        auth = auth,
-        json = { 'is_paused': False })
-response = requests.post(
-        f'{rest_url}/dags/{dag_id}/dagRuns',
-        auth = auth,
-        json = { 'conf': dag_conf })
+response = requests.patch(f'{rest_url}/dags/{dag_id}', auth = auth, json = {'is_paused': False})
+response = requests.post(f'{rest_url}/dags/{dag_id}/dagRuns', auth = auth, json = {'conf': dag_conf})
+
 assert response.status_code == 200, "DAG run could not be triggered."
 
 # Wait for the metrics to be consumed by the statsd-exporter
