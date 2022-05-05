17384e0e18265a864d44010f8b0ef895693e0877
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-05-05 12:11:37 +0200

    wip: refactoring

---
+++ rust/operator-binary/src/airflow_db_controller.rs
@@ -0,0 +1,231 @@
+use crate::util::{env_var_from_secret, get_job_state, JobState};
+
+use snafu::{ResultExt, Snafu};
+use stackable_airflow_crd::airflowdb::{AirflowDB, AirflowDBStatus, AirflowDBStatusCondition};
+use stackable_airflow_crd::AirflowCluster;
+use stackable_operator::{
+    builder::{ContainerBuilder, ObjectMetaBuilder},
+    k8s_openapi::api::{
+        batch::v1::{Job, JobSpec},
+        core::v1::{PodSpec, PodTemplateSpec, Secret},
+    },
+    kube::{
+        runtime::{
+            controller::{Action, Context},
+            reflector::ObjectRef,
+        },
+        ResourceExt,
+    },
+    logging::controller::ReconcilerError,
+};
+use std::{sync::Arc, time::Duration};
+use strum::{EnumDiscriminants, IntoStaticStr};
+
+const FIELD_MANAGER_SCOPE: &str = "airflowcluster";
+
+pub struct Ctx {
+    pub client: stackable_operator::client::Client,
+}
+
+#[derive(Snafu, Debug, EnumDiscriminants)]
+#[strum_discriminants(derive(IntoStaticStr))]
+#[allow(clippy::enum_variant_names)]
+pub enum Error {
+    #[snafu(display("object does not refer to AirflowCluster"))]
+    InvalidAirflowReference,
+    #[snafu(display("could not find object {airflow}"))]
+    FindAirflow {
+        source: stackable_operator::error::Error,
+        airflow: ObjectRef<AirflowCluster>,
+    },
+    #[snafu(display("object defines no version"))]
+    ObjectHasNoVersion,
+    #[snafu(display("failed to apply Job for {}", airflow_db))]
+    ApplyJob {
+        source: stackable_operator::error::Error,
+        airflow_db: ObjectRef<AirflowDB>,
+    },
+    #[snafu(display("failed to update status"))]
+    ApplyStatus {
+        source: stackable_operator::error::Error,
+    },
+    #[snafu(display("object is missing metadata to build owner reference"))]
+    ObjectMissingMetadataForOwnerRef {
+        source: stackable_operator::error::Error,
+    },
+    #[snafu(display("database state is 'initializing' but failed to find job {}", init_job))]
+    GetInitializationJob {
+        source: stackable_operator::error::Error,
+        init_job: ObjectRef<Job>,
+    },
+    #[snafu(display("Failed to check whether the secret ({}) exists", secret))]
+    SecretCheck {
+        source: stackable_operator::error::Error,
+        secret: ObjectRef<Secret>,
+    },
+}
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+impl ReconcilerError for Error {
+    fn category(&self) -> &'static str {
+        ErrorDiscriminants::from(self).into()
+    }
+}
+
+pub async fn reconcile_airflow_db(airflow_db: Arc<AirflowDB>, ctx: Context<Ctx>) -> Result<Action> {
+    tracing::info!("Starting reconcile");
+
+    let client = &ctx.get_ref().client;
+
+    if let Some(ref s) = airflow_db.status {
+        match s.condition {
+            AirflowDBStatusCondition::Pending => {
+                let secret_exists = client
+                    .exists::<Secret>(
+                        &airflow_db.spec.credentials_secret,
+                        airflow_db.namespace().as_deref(),
+                    )
+                    .await
+                    .with_context(|_| {
+                        let mut secret_ref =
+                            ObjectRef::<Secret>::new(&airflow_db.spec.credentials_secret);
+                        if let Some(ns) = airflow_db.namespace() {
+                            secret_ref = secret_ref.within(&ns);
+                        }
+                        SecretCheckSnafu { secret: secret_ref }
+                    })?;
+                if secret_exists {
+                    let job = build_init_job(&airflow_db)?;
+                    client
+                        .apply_patch(FIELD_MANAGER_SCOPE, &job, &job)
+                        .await
+                        .context(ApplyJobSnafu {
+                            airflow_db: ObjectRef::from_obj(&*airflow_db),
+                        })?;
+                    // The job is started, update status to reflect new state
+                    client
+                        .apply_patch_status(FIELD_MANAGER_SCOPE, &*airflow_db, &s.initializing())
+                        .await
+                        .context(ApplyStatusSnafu)?;
+                }
+            }
+            AirflowDBStatusCondition::Initializing => {
+                // In here, check the associated job that is running.
+                // If it is still running, do nothing. If it completed, set status to ready, if it failed, set status to failed.
+                let ns = airflow_db
+                    .namespace()
+                    .unwrap_or_else(|| "default".to_string());
+                let job_name = airflow_db.job_name();
+                let job = client.get::<Job>(&job_name, Some(&ns)).await.context(
+                    GetInitializationJobSnafu {
+                        init_job: ObjectRef::<Job>::new(&job_name).within(&ns),
+                    },
+                )?;
+
+                let new_status = match get_job_state(&job) {
+                    JobState::Complete => Some(s.ready()),
+                    JobState::Failed => Some(s.failed()),
+                    JobState::InProgress => None,
+                };
+
+                if let Some(ns) = new_status {
+                    client
+                        .apply_patch_status(FIELD_MANAGER_SCOPE, &*airflow_db, &ns)
+                        .await
+                        .context(ApplyStatusSnafu)?;
+                }
+            }
+            AirflowDBStatusCondition::Ready => (),
+            AirflowDBStatusCondition::Failed => (),
+        }
+    } else {
+        // Status is none => initialize the status object as "Provisioned"
+        let new_status = AirflowDBStatus::new();
+        client
+            .apply_patch_status(FIELD_MANAGER_SCOPE, &*airflow_db, &new_status)
+            .await
+            .context(ApplyStatusSnafu)?;
+    }
+
+    Ok(Action::await_change())
+}
+
+fn build_init_job(airflow_db: &AirflowDB) -> Result<Job> {
+    let commands = vec![
+        String::from("airflow db init"),
+        String::from("airflow db upgrade"),
+        String::from(
+            "airflow users create \
+                    --username \"$ADMIN_USERNAME\" \
+                    --firstname \"$ADMIN_FIRSTNAME\" \
+                    --lastname \"$ADMIN_LASTNAME\" \
+                    --email \"$ADMIN_EMAIL\" \
+                    --password \"$ADMIN_PASSWORD\" \
+                    --role \"Admin\"",
+        ),
+    ];
+
+    let secret = &airflow_db.spec.credentials_secret;
+
+    let env = vec![
+        env_var_from_secret("SECRET_KEY", secret, "connections.secretKey"),
+        env_var_from_secret(
+            "AIRFLOW__CORE__SQL_ALCHEMY_CONN",
+            secret,
+            "connections.sqlalchemyDatabaseUri",
+        ),
+        env_var_from_secret(
+            "AIRFLOW__CELERY__RESULT_BACKEND",
+            secret,
+            "connections.celeryResultBackend",
+        ),
+        env_var_from_secret("ADMIN_USERNAME", secret, "adminUser.username"),
+        env_var_from_secret("ADMIN_FIRSTNAME", secret, "adminUser.firstname"),
+        env_var_from_secret("ADMIN_LASTNAME", secret, "adminUser.lastname"),
+        env_var_from_secret("ADMIN_EMAIL", secret, "adminUser.email"),
+        env_var_from_secret("ADMIN_PASSWORD", secret, "adminUser.password"),
+    ];
+
+    let container = ContainerBuilder::new("airflow-init-db")
+        .image(format!(
+            "docker.stackable.tech/stackable/airflow:{}-stackable0",
+            airflow_db.spec.airflow_version
+        ))
+        .command(vec!["/bin/bash".to_string()])
+        .args(vec![String::from("-c"), commands.join("; ")])
+        .add_env_vars(env)
+        .build();
+
+    let pod = PodTemplateSpec {
+        metadata: Some(
+            ObjectMetaBuilder::new()
+                .name(format!("{}-init", airflow_db.name()))
+                .build(),
+        ),
+        spec: Some(PodSpec {
+            containers: vec![container],
+            restart_policy: Some("Never".to_string()),
+            ..Default::default()
+        }),
+    };
+
+    let job = Job {
+        metadata: ObjectMetaBuilder::new()
+            .name(airflow_db.name())
+            .namespace_opt(airflow_db.namespace())
+            .ownerreference_from_resource(airflow_db, None, Some(true))
+            .context(ObjectMissingMetadataForOwnerRefSnafu)?
+            .build(),
+        spec: Some(JobSpec {
+            template: pod,
+            ..Default::default()
+        }),
+        status: None,
+    };
+
+    Ok(job)
+}
+
+pub fn error_policy(_error: &Error, _ctx: Context<Ctx>) -> Action {
+    Action::requeue(Duration::from_secs(5))
+}
--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,17 +1,22 @@
 mod airflow_controller;
-mod init_controller;
+mod airflow_db_controller;
+mod util;
 
 use clap::Parser;
 use futures::StreamExt;
-use stackable_airflow_crd::{commands::Init, AirflowCluster};
+use stackable_airflow_crd::{airflowdb::AirflowDB, AirflowCluster, APP_NAME};
 use stackable_operator::cli::ProductOperatorRun;
 use stackable_operator::logging::controller::report_controller_reconciled;
 use stackable_operator::{
     cli::Command,
-    k8s_openapi::api::{apps::v1::StatefulSet, core::v1::Service},
+    k8s_openapi::api::{
+        apps::v1::StatefulSet,
+        batch::v1::Job,
+        core::v1::{Secret, Service},
+    },
     kube::{
         api::ListParams,
-        runtime::{controller::Context, Controller},
+        runtime::{controller::Context, reflector::ObjectRef, Controller},
         CustomResourceExt,
     },
 };
@@ -29,19 +34,18 @@ struct Opts {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    stackable_operator::logging::initialize_logging("AIRFLOW_OPERATOR_LOG");
-
     let opts = Opts::parse();
 
     match opts.cmd {
         Command::Crd => println!(
             "{}{}",
             serde_yaml::to_string(&AirflowCluster::crd())?,
-            serde_yaml::to_string(&Init::crd())?
+            serde_yaml::to_string(&AirflowDB::crd())?
         ),
         Command::Run(ProductOperatorRun {
             product_config,
             watch_namespace,
+            tracing_target,
         }) => {
             stackable_operator::utils::print_startup_string(
                 built_info::PKG_DESCRIPTION,
@@ -51,14 +55,21 @@ async fn main() -> anyhow::Result<()> {
                 built_info::BUILT_TIME_UTC,
                 built_info::RUSTC_VERSION,
             );
+            stackable_operator::logging::initialize_logging(
+                "AIRFLOW_OPERATOR_LOG",
+                APP_NAME,
+                tracing_target,
+            );
             let product_config = product_config.load(&[
                 "deploy/config-spec/properties.yaml",
                 "/etc/stackable/airflow-operator/config-spec/properties.yaml",
             ])?;
+
             let client = stackable_operator::client::create_client(Some(
                 "airflow.stackable.tech".to_string(),
             ))
             .await?;
+
             let airflow_controller = Controller::new(
                 watch_namespace.get_api::<AirflowCluster>(&client),
                 ListParams::default(),
@@ -88,23 +99,65 @@ async fn main() -> anyhow::Result<()> {
                 );
             });
 
-            let init_controller = Controller::new(
-                watch_namespace.get_api::<Init>(&client),
+            let airflow_db_controller_builder = Controller::new(
+                watch_namespace.get_api::<AirflowDB>(&client),
                 ListParams::default(),
-            )
-            .shutdown_on_signal()
-            .run(
-                init_controller::reconcile_init,
-                init_controller::error_policy,
-                Context::new(init_controller::Ctx {
-                    client: client.clone(),
-                }),
-            )
-            .map(|res| {
-                report_controller_reconciled(&client, "inits.command.airflow.stackable.tech", &res);
-            });
+            );
 
-            futures::stream::select(airflow_controller, init_controller)
+            let airflow_db_store1 = airflow_db_controller_builder.store();
+            let airflow_db_store2 = airflow_db_controller_builder.store();
+            let airflow_db_controller = airflow_db_controller_builder
+                .shutdown_on_signal()
+                .watches(
+                    watch_namespace.get_api::<Secret>(&client),
+                    ListParams::default(),
+                    move |secret| {
+                        airflow_db_store1
+                            .state()
+                            .into_iter()
+                            .filter(move |airflow_db| {
+                                if let Some(n) = &secret.metadata.name {
+                                    &airflow_db.spec.credentials_secret == n
+                                } else {
+                                    false
+                                }
+                            })
+                            .map(|airflow_db| ObjectRef::from_obj(&*airflow_db))
+                    },
+                )
+                // We have to watch jobs so we can react to finished init jobs
+                // and update our status accordingly
+                .watches(
+                    watch_namespace.get_api::<Job>(&client),
+                    ListParams::default(),
+                    move |job| {
+                        airflow_db_store2
+                            .state()
+                            .into_iter()
+                            .filter(move |airflow_db| {
+                                airflow_db.metadata.namespace.as_ref().unwrap()
+                                    == job.metadata.namespace.as_ref().unwrap()
+                                    && &airflow_db.job_name() == job.metadata.name.as_ref().unwrap()
+                            })
+                            .map(|airflow_db| ObjectRef::from_obj(&*airflow_db))
+                    },
+                )
+                .run(
+                    airflow_db_controller::reconcile_airflow_db,
+                    airflow_db_controller::error_policy,
+                    Context::new(airflow_db_controller::Ctx {
+                        client: client.clone(),
+                    }),
+                )
+                .map(|res| {
+                    report_controller_reconciled(
+                        &client,
+                        "airflowdbclusters.airflow.stackable.tech",
+                        &res,
+                    )
+                });
+
+            futures::stream::select(airflow_controller, airflow_db_controller)
                 .collect::<()>()
                 .await;
         }
