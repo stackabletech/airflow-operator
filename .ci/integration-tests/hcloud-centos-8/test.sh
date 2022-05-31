0c48f52685cf8cd4751b28c4ff52479a5fc0b4cd
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-05-31 09:15:26 +0200

    move adding of repo declaration outside of tests

--- .ci/integration-tests/hcloud-centos-8/test.sh
+++ .ci/integration-tests/hcloud-centos-8/test.sh
@@ -2,6 +2,7 @@
 
 # Execute tests
 git clone -b "$GIT_BRANCH" https://github.com/stackabletech/airflow-operator.git
+helm repo add bitnami https://charts.bitnami.com/bitnami
 (cd airflow-operator/ && ./scripts/run_tests.sh --parallel 1)
 exit_code=$?
 
