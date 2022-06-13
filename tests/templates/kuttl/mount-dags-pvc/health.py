ad6a6530f7d92a5658681ebc9b34770962fbab3a
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-06-13 12:51:52 +0200

    reverted python script for pvc jobs

--- tests/templates/kuttl/mount-dags-pvc/health.py
+++ tests/templates/kuttl/mount-dags-pvc/health.py
@@ -2,7 +2,6 @@
 import logging
 import requests
 import sys
-import time
 
 if __name__ == "__main__":
     result = 0
@@ -10,27 +9,9 @@ if __name__ == "__main__":
     log_level = 'DEBUG'
     logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
 
-    url = "http://airflow-webserver-default:8080/api/v1/health"
-    count = 1
+    http_code = requests.get("http://airflow-webserver-default:8080/api/v1/health").status_code
 
-    while True:
-        try:
-            count = count + 1
-            res = requests.get(url, timeout=5)
-            code = res.status_code
-            if code == 200:
-                break
-            else:
-                print(f"Got non 200 status code [{code}], retrying attempt no [{count}] ....")
-        except requests.exceptions.Timeout:
-            print(f"Connection timed out, retrying attempt no [{count}] ....")
-        except requests.ConnectionError as e:
-            print(f"Connection Error: {str(e)}")
-        except requests.RequestException as e:
-            print(f"General Error: {str(e)}")
-        except Exception as e:
-            print(f"General error occurred {str(e)}, retrying attempt no [{count}] ....")
-
-        # Wait a little bit before retrying
-        time.sleep(1)
+    if http_code != 200:
+        result = 1
+        
     sys.exit(result)
