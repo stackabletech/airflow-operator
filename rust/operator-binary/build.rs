58b7b389a9adcc591bab38804fab94ed22dab6be
Malte Sander <malte.sander.it@gmail.com>
2023-01-10 11:43:26 +0100

    removed crd generation from build.rs

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,10 +1,3 @@
-use stackable_airflow_crd::airflowdb::AirflowDB;
-use stackable_airflow_crd::AirflowCluster;
-use stackable_operator::crd::CustomResourceExt;
-
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    AirflowCluster::write_yaml_schema("../../deploy/crd/airflowcluster.crd.yaml").unwrap();
-    AirflowDB::write_yaml_schema("../../deploy/crd/airflowdb.crd.yaml").unwrap();
 }
