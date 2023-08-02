4321e779a632391acf6defb984ee44221d397d59
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-08-02 16:44:22 +0200

    fixed merged conflict

--- rust/operator-binary/src/config.rs
+++ rust/operator-binary/src/config.rs
@@ -170,7 +170,7 @@ mod tests {
 
     #[test]
     fn test_ldap() {
-        let input = r#"
+        let authentication_class = "
             apiVersion: authentication.stackable.tech/v1alpha1
             kind: AuthenticationClass
             metadata:
@@ -190,10 +190,11 @@ mod tests {
                       server:
                         caCert:
                           secretClass: openldap-tls
-          "#;
+          ";
 
+        let deserializer = serde_yaml::Deserializer::from_str(authentication_class);
         let authentication_class: AuthenticationClass =
-            serde_yaml::from_str(input).expect("illegal test input");
+            serde_yaml::with::singleton_map_recursive::deserialize(deserializer).unwrap();
 
         let resolved_config = AirflowAuthenticationConfigResolved {
             authentication_class: Some(authentication_class),
