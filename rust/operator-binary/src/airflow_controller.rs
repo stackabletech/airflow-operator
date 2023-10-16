3482417ca01f806b7a5729e451995bab9e0af379
Malte Sander <malte.sander.it@gmail.com>
2023-10-16 14:45:24 +0200

    bump operator-rs to 0.55.0

--- rust/operator-binary/src/airflow_controller.rs
+++ rust/operator-binary/src/airflow_controller.rs
@@ -41,7 +41,6 @@ use stackable_operator::{
         product_image_selection::ResolvedProductImage,
         rbac::build_rbac_resources,
     },
-    duration::Duration,
     k8s_openapi::{
         api::{
             apps::v1::{StatefulSet, StatefulSetSpec},
@@ -69,6 +68,7 @@ use stackable_operator::{
         compute_conditions, operations::ClusterOperationsConditionBuilder,
         statefulset::StatefulSetConditionBuilder,
     },
+    time::Duration,
 };
 use std::{
     collections::{BTreeMap, HashMap},
