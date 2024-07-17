0b0d15eff05c7f983df1f65f51b946e87aadad55
Nick Larsen <nick.larsen@stackable.tech>
2024-07-17 12:36:31 +0200

    chore(ruff): apply formatting

--- tests/templates/kuttl/commons/health.py
+++ tests/templates/kuttl/commons/health.py
@@ -5,8 +5,12 @@ import sys
 import time
 
 if __name__ == "__main__":
-    log_level = 'DEBUG'
-    logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
+    log_level = "DEBUG"
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s %(levelname)s: %(message)s",
+        stream=sys.stdout,
+    )
 
     url = "http://airflow-webserver-default:8080/api/v1/health"
     count = 0
@@ -19,7 +23,9 @@ if __name__ == "__main__":
             if code == 200:
                 break
             else:
-                print(f"Got non 200 status code [{code}], retrying attempt no [{count}] ....")
+                print(
+                    f"Got non 200 status code [{code}], retrying attempt no [{count}] ...."
+                )
         except requests.exceptions.Timeout:
             print(f"Connection timed out, retrying attempt no [{count}] ....")
         except requests.ConnectionError as e:
@@ -27,7 +33,9 @@ if __name__ == "__main__":
         except requests.RequestException as e:
             print(f"General Error: {str(e)}")
         except Exception as e:
-            print(f"General error occurred {str(e)}, retrying attempt no [{count}] ....")
+            print(
+                f"General error occurred {str(e)}, retrying attempt no [{count}] ...."
+            )
 
         # Wait a little bit before retrying
         time.sleep(1)
