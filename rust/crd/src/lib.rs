8ffd43a3c98afc88f1f4bfc15158efb113f3e427
Maxi Wittich <maximilian.wittich@stackable.tech>
2025-01-14 15:12:23 +0100

    Enable Auth_Roles_Mapping as configOverride

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -112,6 +112,7 @@ pub enum AirflowConfigOptions {
     AuthLdapTlsKeyfile,
     AuthLdapTlsCacertfile,
     AuthLdapAllowSelfSigned,
+    AuthRolesMapping,
 }
 
 impl FlaskAppConfigOptions for AirflowConfigOptions {
@@ -137,6 +138,7 @@ impl FlaskAppConfigOptions for AirflowConfigOptions {
             AirflowConfigOptions::AuthLdapTlsKeyfile => PythonType::StringLiteral,
             AirflowConfigOptions::AuthLdapTlsCacertfile => PythonType::StringLiteral,
             AirflowConfigOptions::AuthLdapAllowSelfSigned => PythonType::BoolLiteral,
+            AirflowConfigOptions::AuthRolesMapping => PythonType::StringLiteral,
         }
     }
 }
