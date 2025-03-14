91582f6157371d1f2a946a662152b48e2598b982
Techassi <git@techassi.dev>
2025-03-14 13:53:11 +0100

    chore(docs): Update getting-started script pre-25.3.0 (#595)
    * chore: Bump postgresql and redis chart version
    
    * chore: Use OCI registries for Helm chart installation
    
    * chore: Fix typo

--- docs/modules/airflow/examples/getting_started/code/getting_started.sh
+++ docs/modules/airflow/examples/getting_started/code/getting_started.sh
@@ -18,20 +18,20 @@ then
 fi
 
 echo "Adding bitnami Helm Chart repository and dependencies (Postgresql and Redis)"
-# tag::helm-add-bitnami-repo[]
-helm repo add bitnami https://charts.bitnami.com/bitnami
-# end::helm-add-bitnami-repo[]
 # tag::helm-add-bitnami-pgs[]
-helm install --wait airflow-postgresql bitnami/postgresql --version 12.1.5 \
-    --set auth.username=airflow \
-    --set auth.password=airflow \
-    --set auth.database=airflow
+helm install airflow-postgresql oci://registry-1.docker.io/bitnamicharts/postgresql \
+  --version 16.5.0 \
+  --set auth.database=airflow \
+  --set auth.username=airflow \
+  --set auth.password=airflow \
+  --wait
 # end::helm-add-bitnami-pgs[]
 # tag::helm-add-bitnami-redis[]
-helm install --wait airflow-redis bitnami/redis \
-    --set auth.password=redis \
-    --version 17.3.7 \
-    --set replica.replicaCount=1
+helm install airflow-redis oci://registry-1.docker.io/bitnamicharts/redis \
+  --version 20.11.3 \
+  --set replica.replicaCount=1 \
+  --set auth.password=redis \
+  --wait
 # end::helm-add-bitnami-redis[]
 
 case "$1" in
