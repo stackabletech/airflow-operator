d543b23641d20342c53985c1c5d8dfd941fcb1d8
Andrew Kenworthy <andrew.kenworthy@stackable.tech>
2026-02-05 15:11:42 +0100

    added some comments

--- rust/operator-binary/src/connections/database.rs
+++ rust/operator-binary/src/connections/database.rs
@@ -3,6 +3,22 @@ use std::collections::BTreeMap;
 use serde::{Deserialize, Serialize};
 use stackable_operator::schemars::{self, JsonSchema};
 
+// Struct defining supported backend database types. The implementation should
+// conform to ADR 29. Concrete types will define all fields necessary to
+// construct a connection for that database type. Each backend requires a
+// secret containing `username` and `password`. Additionally, a generic type
+// is defined which will contain a single field pointing to a secret with a
+// single `uri` field that contains the entire connection string. A single
+// backend type may use multiple drivers that are prefixed to the connection
+// string. The operator knows the context of the backend connection (e.g. is it
+// for a SqlALchemyURI- or a Celery-connection) and is therefore responsible
+// for calling the correct function. The connection string will not contain
+// resolved credentials, but rather embedded environment variables that point
+// to values that have been set via the resource definitions (created by the
+// operator). In this way the operator never has to read the secret itself.
+// These embedded values within enviroment variables are resolved when read by
+// product containers, as they are read through a process started within a
+// shell (which performs variable substitution).
 #[derive(Clone, Deserialize, Debug, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub enum DbType {
@@ -40,6 +56,17 @@ impl DbType {
 }
 
 impl PostgresqlDb {
+    // At this point the necessary secrets have been added to the product
+    // statefulset (as env-vars). If a product has been started via a shell,
+    // then any embedded environment variables will be resolved (i.e. variable
+    // substitution) automatically. If for some reason the env-var is accessed
+    // directly via program code (e.g. python), then this will not work, and
+    // the env-vars will instead need to be replaced with the same information
+    // written to a relevant configuration file (as env-vars cannot be safely
+    // templated/re-written from Rust in a multi-threaded environment): in this
+    // case the resulting connection will be written (where env-vars are in the
+    // form of ${{env:...}}) to a file which is then templated on container
+    // start-up.
     fn connection_string(&self, prefix: &str, username_env: &str, password_env: &str) -> String {
         let params = if self.parameters.is_empty() {
             String::new()
