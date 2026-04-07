8c12c9863cf13190df8b5326c1f3b8d9eed1f932
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-04-07 12:28:40 +0200

    rustfmt

--- rust/operator-binary/src/config.rs
+++ rust/operator-binary/src/config.rs
@@ -235,10 +235,9 @@ fn append_oidc_config(
                     .well_known_config_url()
                     .context(InvalidWellKnownConfigUrlSnafu)?;
 
-                let client_auth_method = serde_json::to_value(
-                    client_options.client_authentication_method,
-                )
-                .expect("ClientAuthenticationMethod should serialize to JSON");
+                let client_auth_method =
+                    serde_json::to_value(client_options.client_authentication_method)
+                        .expect("ClientAuthenticationMethod should serialize to JSON");
                 let client_auth_method = client_auth_method
                     .as_str()
                     .expect("ClientAuthenticationMethod should serialize to a string");
